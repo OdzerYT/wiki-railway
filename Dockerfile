9fb455f3b4a3520e46e2d45cce22f7b075a6d859
OdzerYT <weizihanoscar@gmail.com>
2026-08-31 21:59:07 +1200

    Configure MediaWiki for Railway

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,15 @@
 ﻿FROM php:8.3-apache
 
 RUN docker-php-ext-install mysqli
-
 RUN a2enmod rewrite
 
 COPY . /var/www/html/
 
+COPY docker-entrypoint.sh /usr/local/bin/docker-entrypoint.sh
+
+RUN chmod +x /usr/local/bin/docker-entrypoint.sh
 RUN chown -R www-data:www-data /var/www/html
 
 EXPOSE 80
 
-CMD ["apache2-foreground"]
+ENTRYPOINT ["/usr/local/bin/docker-entrypoint.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,11 @@
+﻿#!/bin/bash
+set -e
+
+cp /var/www/html/LocalSettings.php.example /var/www/html/LocalSettings.php
+
+sed -i "s|\$wgDBserver = .*|\$wgDBserver = getenv('MYSQLHOST');|" /var/www/html/LocalSettings.php
+sed -i "s|\$wgDBname = .*|\$wgDBname = getenv('MYSQLDATABASE');|" /var/www/html/LocalSettings.php
+sed -i "s|\$wgDBuser = .*|\$wgDBuser = getenv('MYSQLUSER');|" /var/www/html/LocalSettings.php
+sed -i "s|\$wgDBpassword = .*|\$wgDBpassword = getenv('MYSQLPASSWORD');|" /var/www/html/LocalSettings.php
+
+exec apache2-foreground
